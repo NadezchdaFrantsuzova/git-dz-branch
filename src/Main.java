7ff8b3beb4f1cb48430af18b63400858ba24a32b
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-08-05 20:14:19 +0300

    Добавлены: README.md, Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] sales = {300, 10, 190, 350, 90, 50};
+        SalesManager salesManager = new SalesManager(sales);
+
+        System.out.println("Максимальное количество продаж: " + salesManager.max());
+    }
+}
